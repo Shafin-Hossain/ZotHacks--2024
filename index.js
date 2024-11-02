3e428b755d730f2d661b9b747b3441e9d5d2c6ae
Angelina Ly <lyaw1@uci.edu>
2024-11-02 13:03:18 -0700

    nextpagedone

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ function startNextPage() {
 
     console.log('Executed')
 }
+function questionOneNextPage() {
+    let home = document.getElementById('question-1');
+    home.style.display = 'none';
+
+    let questionOne = document.getElementById('question-2');
+    questionOne.style.display = 'flex';
+
+    console.log('Executed')
+}
+function questionTwoNextPage() {
+    let home = document.getElementById('question-2');
+    home.style.display = 'none';
+
+    let questionOne = document.getElementById('question-3');
+    questionOne.style.display = 'flex';
+
+    console.log('Executed')
+}
+function questionThreeNextPage() {
+    let home = document.getElementById('question-3');
+    home.style.display = 'none';
+
+    let questionOne = document.getElementById('end-screen');
+    questionOne.style.display = 'flex';
+
+    console.log('Executed')
+}

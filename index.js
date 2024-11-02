06d874173cd08d0143a1c02fe41b1e7cb71e1175
Shafin Hossain <shafinhossain106@gmail.com>
2024-11-02 16:16:23 -0700

    Created getParamaters() function

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+import image_api from './image_api.js';
+import quote_api from './quote_api.js';
+
+function getParamaters() {
+    const chosen = localStorage.getItem("1") + localStorage.getItem("2") + localStorage.getItem("3")
+    switch(chosen) {
+        case '111':
+            return ['casual gamer', 'cool']
+        case '112':
+            return ['social gamer', 'humour']
+        case '121':
+            return ['social butterfly', 'friendship']
+        case '122':
+            return ['avid adventurer', 'funny']
+        case '211':
+            return ['homebody', 'home']
+        case '212':
+            return ['nerd', 'intelligence']
+        case '221':
+            return ['laidback', 'home']
+        case '222':
+            return ['charmster', 'sucess']
+    }
+}
+
 function startNextPage() {
     let home = document.getElementById('home');
     home.style.display = 'none';
@@ -64,4 +89,6 @@ function questionThreeNextPage() {
 
     let questionFour = document.getElementById('end-screen');
     questionFour.style.display = 'flex';
+
+    const [imageQuery, quoteQuery] = getParamaters()
 }

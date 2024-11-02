a808c338f4290a4cfb5a539c8ada9d80bfe71714
Shafin Hossain <shafinhossain106@gmail.com>
2024-11-02 12:28:03 -0700

    Home Page Done

---
+++ index.js
@@ -0,0 +1,9 @@
+function startNextPage() {
+    let home = document.getElementById('home');
+    home.style.display = 'none';
+
+    let questionOne = document.getElementById('question-1');
+    questionOne.style.display = 'flex';
+
+    console.log('Executed')
+}
